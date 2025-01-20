84aff3b6879e193032ecd54f25721f374a0c476a
masaki.nakano <bdyht515@icloud.com>
2025-01-20 09:45:17 +0900

    リファクタ（N+1問題の解消）

--- backend/app/controllers/messages_controller.rb
+++ backend/app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: ["index"]
 
   def index
-    messages = Message.all
+    messages = Message.includes(:user, [likes: :user])
     messages_array = messages.map do |message|
       {
         id: message.id,
